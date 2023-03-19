5f2c12aa29e5ff20d4c9c49a92c9283f6a7816e7
jkort <jortealv@myuax.com>
2023-03-19 16:08:54 +0100

    MostarBicicletasFunciona

--- src/main/java/AlquilerBicis/Segway.java
+++ src/main/java/AlquilerBicis/Segway.java
@@ -3,12 +3,12 @@ package AlquilerBicis;
 import java.time.LocalDate;
 
 public class Segway extends Ciclo {
-    private double range;
+    private double autonomia;
     private int alturaMinima;
 
     public Segway(String marca, String modelo, LocalDate fechaCompra, double autonomia, int alturaMinima) {
         super(marca, modelo, fechaCompra);
-        this.range = autonomia;
+        this.autonomia = autonomia;
         this.alturaMinima = alturaMinima;
         this.tarifaAlquiler = 18.90;
     }
@@ -17,6 +17,6 @@ public class Segway extends Ciclo {
     public void mostrarDetalles() {
         System.out.printf("Segways %s %s (%d años) %.0f km de autonomía %s/hora\n",
                 marca, modelo, LocalDate.now().getYear() - fechaCompra.getYear(),
-                range, tarifaAlquiler);
+                autonomia, tarifaAlquiler);
     }
 }
